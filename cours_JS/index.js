4cac2c2a6b72924c7317107572a7a1ce321b4e1b
Loic Jeannin <inklo.ca@gmail.com>
2024-08-18 13:04:49 -0400

    les datas

---
+++ cours_JS/index.js
@@ -0,0 +1,41 @@
+let string = "hello world";
+let number = 25;
+let boolean = true
+
+//les tableaux
+let array = ["loic", 25, true, "julien"]
+//console.log(array[0][3])
+
+let array2 = ["loic", 24, true ,[1,2], {nom: "denis"}]
+//console.log(array2[4].nom)
+
+let objet = {
+  pseudo: "deadpool",
+  age: 38,
+  techno: ["javascript", "SCSS", "python"],
+  admin: false
+}
+//objet.adress = "22 rue du code" => cela va ajouter l'adresse, ou la remplacer si le clé adress existe deja
+
+let data = [
+  {
+    pseudo: "deadpool",
+    age: 38,
+    techno: ["javascript", "SCSS", "python"],
+    admin: false,
+  }
+
+  {
+    pseudo: "julien",
+    age: 42,
+    techno: ["PHP", "SCSS", "python"],
+    admin: true,
+  }
+
+  {
+    pseudo: "Alex",
+    age: 38,
+    techno: ["javascript", "SCSS", "python"],
+    admin: false
+  }
+]
