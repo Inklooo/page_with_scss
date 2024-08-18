fd350d07bacbca5e9beaf1ae67c751a0f75c66a2
Loic Jeannin <inklo.ca@gmail.com>
2024-08-18 15:37:05 -0400

    les structures de controle

--- cours_JS/index.js
+++ cours_JS/index.js
@@ -23,14 +23,14 @@ let data = [
     age: 38,
     techno: ["javascript", "SCSS", "python"],
     admin: false,
-  }
+  },
 
   {
     pseudo: "julien",
     age: 42,
     techno: ["PHP", "SCSS", "python"],
     admin: true,
-  }
+  },
 
   {
     pseudo: "Alex",
@@ -39,3 +39,59 @@ let data = [
     admin: false
   }
 ]
+//les datas vont dans un tableau
+//console.log(data[2].pseudo[0]) => on affiche alex
+
+//-------------------------
+// LES STRUCUTRES DE CONTROLE
+//-------------------------
+
+if(data[0].age > data[1].age) {
+  console.log("deadpool est plus vieux que Julien")
+} else { console.log("Julien est plus vieux que deadpool")
+  };
+
+  //WHILE
+
+  let a = 0;
+
+  while (a < 10) {
+    a ++
+    console.log(a)
+  }
+  // tant que a est inferieur a 10, ajoute 1 à A
+
+  // DO WHILE
+  let b = 0;
+
+  do{
+    b++
+    console.log(b)
+  } while (b < 5);
+
+  //BOUCLE FOR
+  for(const user of data) {
+    //document.body.innerHTML += `<li>${user.pseudo} - ${user.age} ans</li>`
+  }
+
+
+  //on declare la valeur de i / jusuq'ou on boucle / on incrémente i si la condition 2 n'est pas remplie
+  for (i = 0; i < data.length; i++ ) {
+    //console.log(i)
+    //console.log(data[i].techno[0])
+    //document.body.innerHTML += "<h2>" + data[1].techno + "</h2>";
+  };
+
+  //SWITCH
+  document.body.addEventListener("click", (e) =>{
+    switch (e.target.id) {
+      case "javascript":
+        document.body.style.background = "yellow";
+        break;
+      case "php":
+        document.body.style.background = "violet";
+        break;
+      case "python":
+        document.body.style.background = "blue";
+    }
+  });
